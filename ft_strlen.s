bf13a84a4bfb6c40e451117387e4962063c7e3a9
acoudouy <acoudouy@student.42.fr>
2020-02-17 11:05:47 +0100

    strlen et sltrcpy should be ok

---
+++ ft_strlen.s
@@ -0,0 +1,12 @@
+section.text:
+	global _ft_strlen
+
+_ft_strlen:
+	xor rax, rax; #same as mov rax, 0 #initialisation
+_begin_loop:
+	cmp byte [rdi + rax], 0x0;#we compare rdi + rax and NULL
+	jz _stop_loop; #if cmp true  ( = 0) then stop loop
+	inc rax; #we incremente the result
+	jmp _begin_loop
+_stop_loop:
+	ret
---
+++ test.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include "./include/libasm.h"
+
+
+void		test_strlen()
+{
+	printf("\x1b[32mft_strlen :\x1b[0m\n");
+	printf("	\x1b[34m[1]\x1b[0m \x1b[36m''\x1b[0m\n");
+	printf("		\x1b[33m<string.h>\x1b[0m  %lu\n", strlen(""));
+	printf("		\x1b[33m<libasm.h>\x1b[0m  %lu\n", ft_strlen(""));
+	printf("	\x1b[34m[2]\x1b[0m \x1b[36m'    '\x1b[0m\n");
+	printf("		\x1b[33m<string.h>\x1b[0m  %lu\n", strlen("    "));
+	printf("		\x1b[33m<libasm.h>\x1b[0m  %lu\n", ft_strlen("    "));
+	printf("	\x1b[34m[3]\x1b[0m \x1b[36m'0123456789'\x1b[0m\n");
+	printf("		\x1b[33m<string.h>\x1b[0m  %lu\n", strlen("0123456789"));
+	printf("		\x1b[33m<libasm.h>\x1b[0m  %lu\n", ft_strlen("0123456789"));
+	printf("	\x1b[34m[3]\x1b[0m \x1b[36m'NULL'\x1b[0m\n");
+	printf("		\x1b[33m<string.h>\x1b[0m  %s\n", "SEGMENTATION FAULT");
+	printf("		\x1b[33m<libasm.h>\x1b[0m  %s\n", "SEGMENTATION FAULT");
+}
+
+
+int         main()
+{
+    test_strlen();
+    return (0);
+}
